b5e0bba3b5caf8ed9f3e4a8367e32cd1486dd837
Kilexed <imkilexed@gmail.com>
2021-11-06 20:41:59 +0200

    insert

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title> <?= "Новая страница"; ?> </title>
 </head>
 
 <body>
     <?php
-    echo "Hello World";
+    echo "Текущая дата";
+    echo date(DATE_RSS);
+    ?>
+    <br>
+    <?php
+    echo 9 + 3 - 4;
+    echo "Текст"
     ?>
 </body>
 
