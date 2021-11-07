f3d014f0900ff7392f778e91f4f01f51e1dea487
Kilexed <imkilexed@gmail.com>
2021-11-07 13:38:53 +0200

    insert

--- index.php
+++ index.php
@@ -1,35 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?= "Новая страница"; ?> </title>
+    <title>PHP</title>
 </head>
-
 <body>
     <?php
-    echo "Текущая дата";
-    echo date(DATE_RSS);
-    ?>
-    <br>
-    <?php
-    echo 9 + 3 - 4;
-    echo "Текст"
-    ?>
+    $numb = 4; //int
+    //$numb = 45;
+    $number = 10.15; //float
+    $bool = false; //boolean
+    $str = "Переменная"; //string
 
-    <?php
-    if (mt_rand(0,1)){
-        ?>
-        <div style="color: blue;">Синий текст</div>
-        <?php
-    }else {
-        ?>
-        <div style="color: red;">Красный текст</div>
-        <?php
-    }
+    $a = 0.5; //int
+    $b = "0.5"; //string
+
+    echo $a + floatval($b); //floatval - функция переводит к типу данных float
+    
+    echo $str . ': ' . $numb . ',' . $number;
     ?>
 </body>
-
 </html>
